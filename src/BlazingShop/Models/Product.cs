c7ec71ee87ebab13cd5a4425066e697e1e09e4af
Rene B. Pinto <renebentes@yahoo.com.br>
2024-06-24 16:34:40 -0300

    feat(Product): adiciona campos ao modelo de dados

--- src/BlazingShop/Models/Product.cs
+++ src/BlazingShop/Models/Product.cs
@@ -2,27 +2,34 @@
 
 public class Product
 {
-    public Product(int id, string title, decimal price, int categoryId, Category category)
-    {
-        Id = id;
-        Title = title;
-        Price = price;
-        CategoryId = categoryId;
-        Category = category;
-    }
 
     public Product()
     {
 
     }
 
-    public int Id { get; set; }
+    public Product(int id, string title, string description, string image, decimal price, Category category)
+    {
+        Id = id;
+        Title = title;
+        Description = description;
+        Image = image;
+        Price = price;
+        Category = category;
+        CategoryId = category.Id;
+    }
 
-    public string Title { get; set; } = string.Empty;
-
-    public decimal Price { get; set; }
+    public Category Category { get; set; } = new();
 
     public int CategoryId { get; set; }
 
-    public Category Category { get; set; } = new();
+    public string Description { get; set; } = string.Empty;
+
+    public int Id { get; set; }
+
+    public string Image { get; set; } = string.Empty;
+
+    public decimal Price { get; set; }
+
+    public string Title { get; set; } = string.Empty;
 }
