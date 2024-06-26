fea3b149a07f04d688a8e84e7d9a5c5cc0a5a7d9
Rene B. Pinto <renebentes@yahoo.com.br>
2024-06-26 16:07:48 -0300

    feat: configura combo para categorias

---
+++ src/BlazingShop/Products/Common/GetCategoriesQuery.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazingShop.Products.Common;
+
+public sealed class GetCategoriesQuery
+{
+    public int Id { get; }
+
+    public string Title { get; } = string.Empty;
+}
--- src/BlazingShop/Products/CreateProduct/CreateProductPage.razor.cs
+++ src/BlazingShop/Products/CreateProduct/CreateProductPage.razor.cs
@@ -1,8 +1,12 @@
-﻿namespace BlazingShop.Products.CreateProduct;
+﻿using BlazingShop.Products.Common;
+
+namespace BlazingShop.Products.CreateProduct;
 
 public partial class CreateProductPage
 {
     private readonly CreateProductInput _model = new();
+
+    private readonly IEnumerable<GetCategoriesQuery> _categories = [];
 
     async Task HandleSubmitAsync() { }
 }
