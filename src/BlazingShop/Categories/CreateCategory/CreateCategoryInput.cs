23bcd275377020a07979136f138a398a050dfecd
Rene B. Pinto <renebentes@yahoo.com.br>
2024-06-28 11:22:12 -0300

    feat(Category): define estrutura para cadastro de categorias

---
+++ src/BlazingShop/Categories/CreateCategory/CreateCategoryInput.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazingShop.Categories.CreateCategory;
+
+public class CreateCategoryInput
+{
+    [Required(ErrorMessage = "Título é obrigatório")]
+    [MaxLength(50, ErrorMessage = "Título deve ter no máximo 50 caracteres")]
+    [MinLength(5, ErrorMessage = "Título deve ter no mínimo 5 caracteres")]
+    public string Title { get; set; } = string.Empty;
+}
